d470d0fa81a64ec79ca9b72d3a07f55a9f3b7b17
CoreyVeer <corey.veer@gmail.com>
2018-10-22 12:07:25 -0400

    fix

--- website/include/classes.php
+++ website/include/classes.php
@@ -219,9 +219,12 @@ class Application {
               $pageLink = str_replace("login.php", "twofactor.php", $pageLink);
               $to      = $email;
               $subject = 'Confirm your email address';
+              /*$message = "A request has been made to login to your account at https://russellthackston.me. ".
+                  "If you did not make this request, please ignore this message. No other action is necessary. ".
+                  "To confirm your login, please click the following link: $pageLink?id=$validationid"; */
               $message = "A request has been made to login to your account at https://russellthackston.me. ".
                   "If you did not make this request, please ignore this message. No other action is necessary. ".
-                  "To confirm your login, please click the following link: $pageLink?id=$validationid";
+                  "To confirm your login, please use this validation code: $validationid";
               $headers = 'From: webmaster@russellthackston.me' . "\r\n" .
                   'Reply-To: webmaster@russellthackston.me' . "\r\n";
 
