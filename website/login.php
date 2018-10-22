798c3468ab40f1adfcc7003d0286adbb65a2471a
CoreyVeer <corey.veer@gmail.com>
2018-10-22 11:34:08 -0400

    redirect header

--- website/login.php
+++ website/login.php
@@ -42,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if ($result == TRUE) {
 
 		// Redirect the user to the topics page on success
-		header("Location: list.php");
+		header("Location: twofactor.php");
 		exit();
 
 	}
